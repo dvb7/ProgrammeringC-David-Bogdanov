f963abbb9ed6f7f7c122fc3c26abb695d2eb8093
dvb7 <73330595+dvb7@users.noreply.github.com>
2020-11-13 10:37:51 +0100

    Added Angry Dragons

--- DragonbornProject/sketch.js
+++ DragonbornProject/sketch.js
@@ -1,16 +1,53 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	let x = 100;
+function preload() {
+	db = loadJSON("dragonborn.json");
 }
-var date = new Date();
-var timestamp = date.getTime();
-var test = date / 1000;
+
+function setup() {
+	createCanvas(800, 600);
+}
 
 function draw() {
-	fill(200, 0, 150);
-	rect(50, 50, 100, 100);
+	background(150)
+	
+	//Draw the Dragonborn
+	for (i = 0; i <= 5; i ++) {
 
-	text(timestamp, 200, 100);
-	text(test, 200, 300);
-	//test af branch
+		//Decide the color
+		switch (db.dragonborn[i].type) {
+			case ("Black"):
+				fill('rgb(42,52,57)');
+				break;
+			case ("Blue"):
+				fill('rgb(46,88,148)');
+				break;
+			case ("Brass"):
+				fill('rgb(181,166,66)');
+				break;
+			case ("Bronze"):
+				fill('rgb(205,127,50)');
+				break;
+			case ("Copper"):
+				fill('rgb(184,115,51)');
+				break;
+			case ("Gold"):
+				fill('rgb(252,194,0)');
+		}
+
+		//Draw the head
+		strokeWeight(1);
+		ellipse((i+1)*100, 100, 80);
+
+		//Time for eyes
+		fill(0);
+		ellipse(((i+1)*100)-15, 85, 10);
+		ellipse(((i+1)*100)+15, 85, 10);
+
+		//Now with extra Brow(Tm)
+		strokeWeight(2);
+		line(((i+1)*100)-16, 70, ((i+1)*100)-8, 80);
+		line(((i+1)*100)+8, 80, ((i+1)*100)+16, 70);
+
+		//Gimme a mouth, baby
+		
+	}
 }
