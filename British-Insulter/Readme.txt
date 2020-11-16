805c9430ddfd3d6b610bba6a8e572c1f7e5722d1
dvb7 <73330595+dvb7@users.noreply.github.com>
2020-11-16 14:46:56 +0100

    Cleaned up insults and code, added more credit in txt

---
+++ British-Insulter/Readme.txt
@@ -0,0 +1,6 @@
+Credit to the person i copied text to speach from:
+https://dev.to/asaoluelijah/text-to-speech-in-3-lines-of-javascript-b8h
+
+Insults come from The lore and language of schoolchildren
+https://archive.org/details/lorelanguageof00opie/page/n19/mode/2up
+
--- British-Insulter/sketch.js
+++ British-Insulter/sketch.js
@@ -6,14 +6,14 @@ function setup() {
 	createCanvas(600, 600);
 
 	if ('speechSynthesis' in window) {
-		// Speech Synthesis supported 🎉
+		// Speech Synthesis supported
 		speechSynthesis.getVoices().forEach(function(voice) {
   		console.log(voice.name, voice.default ? voice.default :'');
 		});
 
 	}
 	else {
-		// Speech Synthesis Not Supported 😣
+		// Speech Synthesis Not Supported
 		alert("Sorry, your browser doesn't support text to speech!");
 	}
 	   
@@ -25,8 +25,11 @@ function mousePressed() {
 	  if (mouseX >= 275 && mouseX <= 325 && mouseY >= 275 && mouseY <= 325) {
 		//Speak
 		var msg = new SpeechSynthesisUtterance();
-		var voices = window.speechSynthesis.getVoices();
-		msg.voice = voices[3]; 
+		
+		// Makes my computer use japanese voice, but depends on the computer
+		// var voices = window.speechSynthesis.getVoices();
+		// msg.voice = voices[3]; 
+		
 		msg.text = word.insults[Math.floor(Math.random() * word.insults.length)];
 		window.speechSynthesis.speak(msg);
 	  }
@@ -39,7 +42,4 @@ function draw() {
 	//Draw the button
 	fill('rgb(50,205,50)')
 	rect(275, 275, 50, 50, 5)
-
-
-
 }
