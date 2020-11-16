274e334ee28146a825d91f13e10c6933dc6b7b13
dvb7 <73330595+dvb7@users.noreply.github.com>
2020-11-16 14:41:02 +0100

    Initialised insulter

--- British-Insulter/sketch.js
+++ British-Insulter/sketch.js
@@ -1,16 +1,45 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	let x = 100;
+function preload() {
+	word = loadJSON("insults.json");
 }
-var date = new Date();
-var timestamp = date.getTime();
-var test = date / 1000;
+
+function setup() {
+	createCanvas(600, 600);
+
+	if ('speechSynthesis' in window) {
+		// Speech Synthesis supported 🎉
+		speechSynthesis.getVoices().forEach(function(voice) {
+  		console.log(voice.name, voice.default ? voice.default :'');
+		});
+
+	}
+	else {
+		// Speech Synthesis Not Supported 😣
+		alert("Sorry, your browser doesn't support text to speech!");
+	}
+	   
+}
+
+function mousePressed() {
+	if (mouseButton === LEFT) {
+	  //Is button pressed?
+	  if (mouseX >= 275 && mouseX <= 325 && mouseY >= 275 && mouseY <= 325) {
+		//Speak
+		var msg = new SpeechSynthesisUtterance();
+		var voices = window.speechSynthesis.getVoices();
+		msg.voice = voices[3]; 
+		msg.text = word.insults[Math.floor(Math.random() * word.insults.length)];
+		window.speechSynthesis.speak(msg);
+	  }
+	}
+}
 
 function draw() {
-	fill(200, 0, 150);
-	rect(50, 50, 100, 100);
+	background(150);
 
-	text(timestamp, 200, 100);
-	text(test, 200, 300);
-	//test af branch
+	//Draw the button
+	fill('rgb(50,205,50)')
+	rect(275, 275, 50, 50, 5)
+
+
+
 }
