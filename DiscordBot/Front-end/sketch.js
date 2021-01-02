2f45fb507011f35ee80dd0ecab5745544aa49777
dvb7 <73330595+dvb7@users.noreply.github.com>
2021-01-02 18:40:29 +0100

    Discordbot init comit

---
+++ DiscordBot/Front-end/sketch.js
@@ -0,0 +1,25 @@
+var bgcolor = "red";
+
+function setup() {
+	createCanvas(200, 200);
+	
+}
+
+function draw() {
+
+	switch (bgcolor) {
+		case "red":
+			background('rgb(200,0,0');
+			break;
+		
+		case "blue":
+			background('rgb(46,88,148)');
+			break;
+
+		case "yellow":
+			background('rgb(252,194,0)')
+	}
+	
+	
+	
+}
