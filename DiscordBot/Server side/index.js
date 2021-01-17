3c3d17a4f4da051d1128aa86c4603ffd052c6447
dvb7 <73330595+dvb7@users.noreply.github.com>
2021-01-17 13:31:27 +0100

    Complete program overhaul - can't figure out fetch()

---
+++ DiscordBot/Server side/index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+//Variable setup
+let vchange = -50;
+let hchange = 50;
+
+//Setup the discord bot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.BOTTOKEN);
+
+//Setup Express
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+
+//Make the bot do something, if a message is sent.
+client.on('message', gotMessage);
+
+//Check the content of a message that is sent, and react if neccesary
+function gotMessage(msg) {
+    if (msg.channel.id == process.env.CHANNELID) {
+        switch (msg.content) {
+            case "!up":
+                msg.channel.send("Moving up!");
+                vchange -= 5;
+                break;
+            case "!down":
+                msg.channel.send("Moving down!");
+                vchange += 5;
+                break;
+            case "!left":
+                msg.channel.send("Moving left!");
+                hchange -= 5;
+                break;
+            case "!right":
+                msg.channel.send("Moving right!");
+                hchange += 5;
+        }
+    }
+}
+
+let allData = [ vchange, hchange ];
+
+app.post('/api', (response) => {
+    response.json(allData);
+    console.log(allData);
+  });
