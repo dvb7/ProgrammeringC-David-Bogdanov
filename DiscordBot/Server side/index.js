2a8c1b30132f2e885730da5f3a450d589fae4b7c
dvb7 <73330595+dvb7@users.noreply.github.com>
2021-03-19 15:11:36 +0100

    Added database system on server side

--- DiscordBot/Server side/index.js
+++ DiscordBot/Server side/index.js
@@ -2,9 +2,10 @@
 require("dotenv").config();
 
 //Variable setup
-let vchange = -50;
-let hchange = 50;
+obj = { vchange:-50, hchange:50 }
+let data;
 
+const { info } = require("console");
 //Setup the discord bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -16,6 +17,10 @@ const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 
+//Setup fs
+var fs = require("fs");
+var logger = "|SERVER LOG:| "
+
 //Make the bot do something, if a message is sent.
 client.on('message', gotMessage);
 
@@ -25,23 +30,40 @@ function gotMessage(msg) {
         switch (msg.content) {
             case "!up":
                 msg.channel.send("Moving up!");
-                vchange -= 5;
+                obj.vchange -= 5;
                 break;
             case "!down":
                 msg.channel.send("Moving down!");
-                vchange += 5;
+                obj.vchange += 5;
                 break;
             case "!left":
                 msg.channel.send("Moving left!");
-                hchange -= 5;
+                obj.hchange -= 5;
                 break;
             case "!right":
                 msg.channel.send("Moving right!");
-                hchange += 5;
+                obj.hchange += 5;
+                break;
+            case "!upupdowndownleftrightleftrightb-a-b-astart":
+                msg.channel.send("Secret unlocked! 🥳🎉");
         }
+
+        let data = JSON.stringify(obj)
+        createFile(data, "database")
     }
 }
 
+
+
+function createFile(input, type){
+    fs.writeFile("./../Server side/Client side/#Input/" + type + ".json", input, (err) => {
+        // Rest of your code
+        if (err) throw err;               
+        console.log(logger + "FILE SENT");
+    })
+}
+
+/*
 //Make the variables available, 
 //and send them if anybody tries to fetch them.
 app.post('/api', (response) => {
@@ -49,3 +71,4 @@ app.post('/api', (response) => {
     response.json(allData);
     console.log(allData, "data sent");
   });
+*/
